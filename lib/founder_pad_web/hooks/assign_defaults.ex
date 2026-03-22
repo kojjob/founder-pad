d9ff35ce14a206df2b82a7d38cc22d33556776bc
Kojo <kodeon6@gmail.com>
2026-03-22 00:52:53 +0000

    feat(hooks): add onboarding_complete detection to AssignDefaults

--- lib/founder_pad_web/hooks/assign_defaults.ex
+++ lib/founder_pad_web/hooks/assign_defaults.ex
@@ -1,22 +1,38 @@
 defmodule FounderPadWeb.Hooks.AssignDefaults do
   @moduledoc """
   LiveView on_mount hook that assigns default values needed by the app layout.
-  Loads the current_user from the session token.
+  Loads the current_user from the session token and checks onboarding status.
   """
   import Phoenix.Component, only: [assign: 2]
 
+  require Ash.Query
+
   def on_mount(:default, _params, session, socket) do
-    socket = assign(socket, active_nav: :dashboard)
+    socket = assign(socket, active_nav: :dashboard, setup_banner_dismissed: false)
 
     case session["user_token"] do
       nil ->
-        {:cont, assign(socket, current_user: nil)}
+        {:cont, assign(socket, current_user: nil, onboarding_complete: false)}
 
       token ->
         case AshAuthentication.subject_to_user(token, FounderPad.Accounts.User) do
-          {:ok, user} -> {:cont, assign(socket, current_user: user)}
-          _ -> {:cont, assign(socket, current_user: nil)}
+          {:ok, user} ->
+            onboarding_complete = has_membership?(user.id)
+            {:cont, assign(socket, current_user: user, onboarding_complete: onboarding_complete)}
+
+          _ ->
+            {:cont, assign(socket, current_user: nil, onboarding_complete: false)}
         end
+    end
+  end
+
+  defp has_membership?(user_id) do
+    case FounderPad.Accounts.Membership
+         |> Ash.Query.filter(user_id: user_id)
+         |> Ash.Query.limit(1)
+         |> Ash.read() do
+      {:ok, [_ | _]} -> true
+      _ -> false
     end
   end
 end
